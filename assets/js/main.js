2c250ac92ef81b21f1328a31b40773ca5e18d411
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-05-30 23:26:25 +0100

    Added lazy-load syntax highlighting

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,16 @@ bodyCopySans.load().then(()   => rootElement.classList.add('body-copy-loaded'), 
 headline.load().then(()       => rootElement.classList.add('headline-loaded'), () => {});
 bodySansBold.load().then(()   => rootElement.classList.add('body-bold-loaded'), () => {});
 bodySansItalic.load().then(() => rootElement.classList.add('body-italic-loaded'), () => {});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const syntaxHighlightBlocks = document.querySelectorAll('[data-syntaxhighlight]');
+
+  // lazy load syntax highlighting if required
+  // this approach saves both bytes and DOM traversal, as Prism doesn't
+  // need to re-scan the entire DOM.
+  if (syntaxHighlightBlocks) {
+    loadjs('/assets/js/syntax.js', () => {
+      for (var block of syntaxHighlightBlocks) Prism.highlightElement(block);
+    });
+  }
+});

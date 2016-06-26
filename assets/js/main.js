35c046f8e6798c76b301888d7767bbab987614cd
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-06-26 17:07:49 +0100

    Use new font names in fontfaceobserver

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,16 @@ import FontFaceObserver from 'fontfaceobserver';
 
 const rootElement = document.documentElement;
 
-const headline       = new FontFaceObserver('Alegreya');
-const bodyCopySans   = new FontFaceObserver('Alegreya Sans');
-const bodySansBold   = new FontFaceObserver('Alegreya Sans Bold');
-const bodySansItalic = new FontFaceObserver('Alegreya Sans Italic');
+const headline       = new FontFaceObserver('Roboto Condensed');
+const bodyCopy   = new FontFaceObserver('Alegreya ');
+const bodyBold   = new FontFaceObserver('Alegreya  Bold');
+const bodyItalic = new FontFaceObserver('Alegreya  Italic');
 
 // This slightly weird , () => {}); at the end just swallows timeout errors on slow networks
-bodyCopySans.load().then(()   => rootElement.classList.add('body-copy-loaded'), () => {});
+bodyCopy.load().then(()   => rootElement.classList.add('body-copy-loaded'), () => {});
 headline.load().then(()       => rootElement.classList.add('headline-loaded'), () => {});
-bodySansBold.load().then(()   => rootElement.classList.add('body-bold-loaded'), () => {});
-bodySansItalic.load().then(() => rootElement.classList.add('body-italic-loaded'), () => {});
+bodyBold.load().then(()   => rootElement.classList.add('body-bold-loaded'), () => {});
+bodyItalic.load().then(() => rootElement.classList.add('body-italic-loaded'), () => {});
 
 document.addEventListener('DOMContentLoaded', () => {
 

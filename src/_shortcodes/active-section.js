30fb46e5896bc982eb580a7937594a98fbaa4e89
Alex Jegtnes <alex@jegtnes.com>
2024-02-08 01:02:53 +0000

    Update nav active state

--- src/_shortcodes/active-section.js
+++ src/_shortcodes/active-section.js
@@ -1,8 +1,8 @@
-module.exports = function activeSection(itemUrl, pageUrl) {
+module.exports = function activeSection(itemUrl, pageUrl, output) {
   // The homepage will not have an active state in either case
   if (itemUrl === "/") return "";
 
   if (!pageUrl.startsWith(itemUrl)) return "";
 
-  return "current";
+  return output || "current";
 };

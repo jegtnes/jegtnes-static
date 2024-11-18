5e4c27b1db492a4788af789947a95adfbe7d49d0
Alex Jegtnes <alex@jegtnes.com>
2024-11-18 18:51:53 +0800

    Update remote status

--- src/_shortcodes/availability.js
+++ src/_shortcodes/availability.js
@@ -7,7 +7,7 @@ module.exports = function availability(availability, availabilityString, short =
 
 	if (!availability) return "I presently have no availability for new engagements.";
 
-	if(availabilityString && availabilityString.length) return `<strong>I have availability for new remote engagements from ${availabilityString}</strong>.`;
+	if(availabilityString && availabilityString.length) return `<strong>I have availability for new engagements from ${availabilityString}</strong>, either remote or on-site in London.`;
   
-	return "<strong>I have current availability for new remote engagements</strong>.";
+	return "<strong>I have current availability for new engagements</strong>, either remote or on-site in London.";
 };
